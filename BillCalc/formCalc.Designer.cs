308170e9f080cd2c3e1f44ab453423d79c164d04
Jayson Rowe <jayson.rowe@gmail.com>
2012-09-15 14:52:06 -0400

    Initial Commit

---
+++ BillCalc/formCalc.Designer.cs
@@ -0,0 +1,436 @@
+﻿namespace BillCalc
+{
+    partial class formCalc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.balanceBox = new System.Windows.Forms.TextBox();
+            this.rentBox = new System.Windows.Forms.TextBox();
+            this.lightBox = new System.Windows.Forms.TextBox();
+            this.cableBox = new System.Windows.Forms.TextBox();
+            this.waterBox = new System.Windows.Forms.TextBox();
+            this.cellBox = new System.Windows.Forms.TextBox();
+            this.carBox1 = new System.Windows.Forms.TextBox();
+            this.carBox2 = new System.Windows.Forms.TextBox();
+            this.otherLabel = new System.Windows.Forms.Label();
+            this.rentLabel = new System.Windows.Forms.Label();
+            this.lightLabel = new System.Windows.Forms.Label();
+            this.cableLabel = new System.Windows.Forms.Label();
+            this.waterLabel = new System.Windows.Forms.Label();
+            this.carLabel1 = new System.Windows.Forms.Label();
+            this.carLabel2 = new System.Windows.Forms.Label();
+            this.startingbalLabel = new System.Windows.Forms.Label();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.insBox = new System.Windows.Forms.TextBox();
+            this.cellLabel = new System.Windows.Forms.Label();
+            this.otherBox = new System.Windows.Forms.TextBox();
+            this.insLabel = new System.Windows.Forms.Label();
+            this.dollarLabel = new System.Windows.Forms.Label();
+            this.dollarLabel1 = new System.Windows.Forms.Label();
+            this.dollarLabel2 = new System.Windows.Forms.Label();
+            this.dollarLabel3 = new System.Windows.Forms.Label();
+            this.dollarLabel4 = new System.Windows.Forms.Label();
+            this.dollarLabel5 = new System.Windows.Forms.Label();
+            this.dollarLabel6 = new System.Windows.Forms.Label();
+            this.dollarLabel7 = new System.Windows.Forms.Label();
+            this.dollarLabel8 = new System.Windows.Forms.Label();
+            this.dollarLabel9 = new System.Windows.Forms.Label();
+            this.dollarLabel10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // balanceBox
+            // 
+            this.balanceBox.Location = new System.Drawing.Point(30, 50);
+            this.balanceBox.MaxLength = 15;
+            this.balanceBox.Name = "balanceBox";
+            this.balanceBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.balanceBox.Size = new System.Drawing.Size(160, 20);
+            this.balanceBox.TabIndex = 0;
+            // 
+            // rentBox
+            // 
+            this.rentBox.Location = new System.Drawing.Point(30, 112);
+            this.rentBox.MaxLength = 15;
+            this.rentBox.Name = "rentBox";
+            this.rentBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.rentBox.Size = new System.Drawing.Size(160, 20);
+            this.rentBox.TabIndex = 1;
+            // 
+            // lightBox
+            // 
+            this.lightBox.Location = new System.Drawing.Point(30, 138);
+            this.lightBox.MaxLength = 15;
+            this.lightBox.Name = "lightBox";
+            this.lightBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lightBox.Size = new System.Drawing.Size(160, 20);
+            this.lightBox.TabIndex = 2;
+            // 
+            // cableBox
+            // 
+            this.cableBox.Location = new System.Drawing.Point(30, 164);
+            this.cableBox.MaxLength = 15;
+            this.cableBox.Name = "cableBox";
+            this.cableBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.cableBox.Size = new System.Drawing.Size(160, 20);
+            this.cableBox.TabIndex = 3;
+            // 
+            // waterBox
+            // 
+            this.waterBox.Location = new System.Drawing.Point(30, 190);
+            this.waterBox.MaxLength = 15;
+            this.waterBox.Name = "waterBox";
+            this.waterBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.waterBox.Size = new System.Drawing.Size(160, 20);
+            this.waterBox.TabIndex = 4;
+            // 
+            // cellBox
+            // 
+            this.cellBox.Location = new System.Drawing.Point(30, 216);
+            this.cellBox.MaxLength = 15;
+            this.cellBox.Name = "cellBox";
+            this.cellBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.cellBox.Size = new System.Drawing.Size(160, 20);
+            this.cellBox.TabIndex = 5;
+            // 
+            // carBox1
+            // 
+            this.carBox1.Location = new System.Drawing.Point(30, 242);
+            this.carBox1.MaxLength = 15;
+            this.carBox1.Name = "carBox1";
+            this.carBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.carBox1.Size = new System.Drawing.Size(160, 20);
+            this.carBox1.TabIndex = 6;
+            // 
+            // carBox2
+            // 
+            this.carBox2.Location = new System.Drawing.Point(30, 268);
+            this.carBox2.MaxLength = 15;
+            this.carBox2.Name = "carBox2";
+            this.carBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.carBox2.Size = new System.Drawing.Size(160, 20);
+            this.carBox2.TabIndex = 7;
+            // 
+            // otherLabel
+            // 
+            this.otherLabel.AutoSize = true;
+            this.otherLabel.Location = new System.Drawing.Point(196, 327);
+            this.otherLabel.Name = "otherLabel";
+            this.otherLabel.Size = new System.Drawing.Size(33, 13);
+            this.otherLabel.TabIndex = 8;
+            this.otherLabel.Text = "Other";
+            // 
+            // rentLabel
+            // 
+            this.rentLabel.AutoSize = true;
+            this.rentLabel.Location = new System.Drawing.Point(197, 119);
+            this.rentLabel.Name = "rentLabel";
+            this.rentLabel.Size = new System.Drawing.Size(30, 13);
+            this.rentLabel.TabIndex = 9;
+            this.rentLabel.Text = "Rent";
+            // 
+            // lightLabel
+            // 
+            this.lightLabel.AutoSize = true;
+            this.lightLabel.Location = new System.Drawing.Point(197, 145);
+            this.lightLabel.Name = "lightLabel";
+            this.lightLabel.Size = new System.Drawing.Size(46, 13);
+            this.lightLabel.TabIndex = 10;
+            this.lightLabel.Text = "Light Bill";
+            // 
+            // cableLabel
+            // 
+            this.cableLabel.AutoSize = true;
+            this.cableLabel.Location = new System.Drawing.Point(197, 171);
+            this.cableLabel.Name = "cableLabel";
+            this.cableLabel.Size = new System.Drawing.Size(50, 13);
+            this.cableLabel.TabIndex = 11;
+            this.cableLabel.Text = "Cable Bill";
+            // 
+            // waterLabel
+            // 
+            this.waterLabel.AutoSize = true;
+            this.waterLabel.Location = new System.Drawing.Point(196, 197);
+            this.waterLabel.Name = "waterLabel";
+            this.waterLabel.Size = new System.Drawing.Size(52, 13);
+            this.waterLabel.TabIndex = 12;
+            this.waterLabel.Text = "Water Bill";
+            // 
+            // carLabel1
+            // 
+            this.carLabel1.AutoSize = true;
+            this.carLabel1.Location = new System.Drawing.Point(196, 249);
+            this.carLabel1.Name = "carLabel1";
+            this.carLabel1.Size = new System.Drawing.Size(76, 13);
+            this.carLabel1.TabIndex = 13;
+            this.carLabel1.Text = "Car Payment 1";
+            // 
+            // carLabel2
+            // 
+            this.carLabel2.AutoSize = true;
+            this.carLabel2.Location = new System.Drawing.Point(196, 275);
+            this.carLabel2.Name = "carLabel2";
+            this.carLabel2.Size = new System.Drawing.Size(76, 13);
+            this.carLabel2.TabIndex = 14;
+            this.carLabel2.Text = "Car Payment 2";
+            // 
+            // startingbalLabel
+            // 
+            this.startingbalLabel.AutoSize = true;
+            this.startingbalLabel.Location = new System.Drawing.Point(207, 57);
+            this.startingbalLabel.Name = "startingbalLabel";
+            this.startingbalLabel.Size = new System.Drawing.Size(128, 13);
+            this.startingbalLabel.TabIndex = 17;
+            this.startingbalLabel.Text = "Starting Account Balance";
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(30, 346);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(75, 23);
+            this.calcButton.TabIndex = 10;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // insBox
+            // 
+            this.insBox.Location = new System.Drawing.Point(30, 294);
+            this.insBox.MaxLength = 15;
+            this.insBox.Name = "insBox";
+            this.insBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.insBox.Size = new System.Drawing.Size(160, 20);
+            this.insBox.TabIndex = 8;
+            // 
+            // cellLabel
+            // 
+            this.cellLabel.AutoSize = true;
+            this.cellLabel.Location = new System.Drawing.Point(196, 223);
+            this.cellLabel.Name = "cellLabel";
+            this.cellLabel.Size = new System.Drawing.Size(58, 13);
+            this.cellLabel.TabIndex = 20;
+            this.cellLabel.Text = "Cell Phone";
+            // 
+            // otherBox
+            // 
+            this.otherBox.Location = new System.Drawing.Point(30, 320);
+            this.otherBox.MaxLength = 15;
+            this.otherBox.Name = "otherBox";
+            this.otherBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.otherBox.Size = new System.Drawing.Size(160, 20);
+            this.otherBox.TabIndex = 9;
+            // 
+            // insLabel
+            // 
+            this.insLabel.AutoSize = true;
+            this.insLabel.Location = new System.Drawing.Point(197, 301);
+            this.insLabel.Name = "insLabel";
+            this.insLabel.Size = new System.Drawing.Size(73, 13);
+            this.insLabel.TabIndex = 22;
+            this.insLabel.Text = "Car Insurance";
+            // 
+            // dollarLabel
+            // 
+            this.dollarLabel.AutoSize = true;
+            this.dollarLabel.Location = new System.Drawing.Point(-11, 56);
+            this.dollarLabel.Name = "dollarLabel";
+            this.dollarLabel.Size = new System.Drawing.Size(13, 13);
+            this.dollarLabel.TabIndex = 23;
+            this.dollarLabel.Text = "$";
+            // 
+            // dollarLabel1
+            // 
+            this.dollarLabel1.AutoSize = true;
+            this.dollarLabel1.Location = new System.Drawing.Point(12, 57);
+            this.dollarLabel1.Name = "dollarLabel1";
+            this.dollarLabel1.Size = new System.Drawing.Size(13, 13);
+            this.dollarLabel1.TabIndex = 24;
+            this.dollarLabel1.Text = "$";
+            // 
+            // dollarLabel2
+            // 
+            this.dollarLabel2.AutoSize = true;
+            this.dollarLabel2.Location = new System.Drawing.Point(12, 115);
+            this.dollarLabel2.Name = "dollarLabel2";
+            this.dollarLabel2.Size = new System.Drawing.Size(13, 13);
+            this.dollarLabel2.TabIndex = 25;
+            this.dollarLabel2.Text = "$";
+            // 
+            // dollarLabel3
+            // 
+            this.dollarLabel3.AutoSize = true;
+            this.dollarLabel3.Location = new System.Drawing.Point(12, 145);
+            this.dollarLabel3.Name = "dollarLabel3";
+            this.dollarLabel3.Size = new System.Drawing.Size(13, 13);
+            this.dollarLabel3.TabIndex = 26;
+            this.dollarLabel3.Text = "$";
+            // 
+            // dollarLabel4
+            // 
+            this.dollarLabel4.AutoSize = true;
+            this.dollarLabel4.Location = new System.Drawing.Point(12, 171);
+            this.dollarLabel4.Name = "dollarLabel4";
+            this.dollarLabel4.Size = new System.Drawing.Size(13, 13);
+            this.dollarLabel4.TabIndex = 27;
+            this.dollarLabel4.Text = "$";
+            // 
+            // dollarLabel5
+            // 
+            this.dollarLabel5.AutoSize = true;
+            this.dollarLabel5.Location = new System.Drawing.Point(12, 193);
+            this.dollarLabel5.Name = "dollarLabel5";
+            this.dollarLabel5.Size = new System.Drawing.Size(13, 13);
+            this.dollarLabel5.TabIndex = 28;
+            this.dollarLabel5.Text = "$";
+            // 
+            // dollarLabel6
+            // 
+            this.dollarLabel6.AutoSize = true;
+            this.dollarLabel6.Location = new System.Drawing.Point(12, 219);
+            this.dollarLabel6.Name = "dollarLabel6";
+            this.dollarLabel6.Size = new System.Drawing.Size(13, 13);
+            this.dollarLabel6.TabIndex = 29;
+            this.dollarLabel6.Text = "$";
+            // 
+            // dollarLabel7
+            // 
+            this.dollarLabel7.AutoSize = true;
+            this.dollarLabel7.Location = new System.Drawing.Point(12, 245);
+            this.dollarLabel7.Name = "dollarLabel7";
+            this.dollarLabel7.Size = new System.Drawing.Size(13, 13);
+            this.dollarLabel7.TabIndex = 30;
+            this.dollarLabel7.Text = "$";
+            // 
+            // dollarLabel8
+            // 
+            this.dollarLabel8.AutoSize = true;
+            this.dollarLabel8.Location = new System.Drawing.Point(12, 268);
+            this.dollarLabel8.Name = "dollarLabel8";
+            this.dollarLabel8.Size = new System.Drawing.Size(13, 13);
+            this.dollarLabel8.TabIndex = 31;
+            this.dollarLabel8.Text = "$";
+            // 
+            // dollarLabel9
+            // 
+            this.dollarLabel9.AutoSize = true;
+            this.dollarLabel9.Location = new System.Drawing.Point(12, 297);
+            this.dollarLabel9.Name = "dollarLabel9";
+            this.dollarLabel9.Size = new System.Drawing.Size(13, 13);
+            this.dollarLabel9.TabIndex = 32;
+            this.dollarLabel9.Text = "$";
+            // 
+            // dollarLabel10
+            // 
+            this.dollarLabel10.AutoSize = true;
+            this.dollarLabel10.Location = new System.Drawing.Point(12, 327);
+            this.dollarLabel10.Name = "dollarLabel10";
+            this.dollarLabel10.Size = new System.Drawing.Size(13, 13);
+            this.dollarLabel10.TabIndex = 33;
+            this.dollarLabel10.Text = "$";
+            // 
+            // formCalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 395);
+            this.Controls.Add(this.dollarLabel10);
+            this.Controls.Add(this.dollarLabel9);
+            this.Controls.Add(this.dollarLabel8);
+            this.Controls.Add(this.dollarLabel7);
+            this.Controls.Add(this.dollarLabel6);
+            this.Controls.Add(this.dollarLabel5);
+            this.Controls.Add(this.dollarLabel4);
+            this.Controls.Add(this.dollarLabel3);
+            this.Controls.Add(this.dollarLabel2);
+            this.Controls.Add(this.dollarLabel1);
+            this.Controls.Add(this.dollarLabel);
+            this.Controls.Add(this.insLabel);
+            this.Controls.Add(this.otherBox);
+            this.Controls.Add(this.cellLabel);
+            this.Controls.Add(this.insBox);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.startingbalLabel);
+            this.Controls.Add(this.carLabel2);
+            this.Controls.Add(this.carLabel1);
+            this.Controls.Add(this.waterLabel);
+            this.Controls.Add(this.cableLabel);
+            this.Controls.Add(this.lightLabel);
+            this.Controls.Add(this.rentLabel);
+            this.Controls.Add(this.otherLabel);
+            this.Controls.Add(this.carBox2);
+            this.Controls.Add(this.carBox1);
+            this.Controls.Add(this.cellBox);
+            this.Controls.Add(this.waterBox);
+            this.Controls.Add(this.cableBox);
+            this.Controls.Add(this.lightBox);
+            this.Controls.Add(this.rentBox);
+            this.Controls.Add(this.balanceBox);
+            this.Name = "formCalc";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "Bill Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox balanceBox;
+        public System.Windows.Forms.TextBox rentBox;
+        public System.Windows.Forms.TextBox lightBox;
+        public System.Windows.Forms.TextBox cableBox;
+        public System.Windows.Forms.TextBox waterBox;
+        public System.Windows.Forms.TextBox cellBox;
+        public System.Windows.Forms.TextBox carBox1;
+        public System.Windows.Forms.TextBox carBox2;
+        private System.Windows.Forms.Label otherLabel;
+        private System.Windows.Forms.Label rentLabel;
+        private System.Windows.Forms.Label lightLabel;
+        private System.Windows.Forms.Label cableLabel;
+        private System.Windows.Forms.Label waterLabel;
+        private System.Windows.Forms.Label carLabel1;
+        private System.Windows.Forms.Label carLabel2;
+        private System.Windows.Forms.Label startingbalLabel;
+        private System.Windows.Forms.Button calcButton;
+        public System.Windows.Forms.TextBox insBox;
+        private System.Windows.Forms.Label cellLabel;
+        public System.Windows.Forms.TextBox otherBox;
+        private System.Windows.Forms.Label insLabel;
+        private System.Windows.Forms.Label dollarLabel;
+        private System.Windows.Forms.Label dollarLabel1;
+        private System.Windows.Forms.Label dollarLabel2;
+        private System.Windows.Forms.Label dollarLabel3;
+        private System.Windows.Forms.Label dollarLabel4;
+        private System.Windows.Forms.Label dollarLabel5;
+        private System.Windows.Forms.Label dollarLabel6;
+        private System.Windows.Forms.Label dollarLabel7;
+        private System.Windows.Forms.Label dollarLabel8;
+        private System.Windows.Forms.Label dollarLabel9;
+        private System.Windows.Forms.Label dollarLabel10;
+
+    }
+}
+
---
+++ BillCalc/formCalc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BillCalc
+{
+    public partial class formCalc : Form
+    {
+        public formCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void resultBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            string balance =
+            (Convert.ToDouble(balanceBox.Text) - Convert.ToDouble(rentBox.Text) - Convert.ToDouble(lightBox.Text) -
+             Convert.ToDouble(cableBox.Text) - Convert.ToDouble(waterBox.Text) - Convert.ToDouble(cellBox.Text) - Convert.ToDouble(carBox1.Text) -
+             Convert.ToDouble(carBox2.Text) - Convert.ToDouble(otherBox.Text)).ToString();
+            MessageBox.Show("$" + balance);
+        }
+    }
+}
